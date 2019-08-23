48cbf0d04d230dc2298ae86466fa418a6916744e
Catherine Clause <cnclause123@gmail.com>
2019-08-23 20:43:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,11 +36,8 @@ def monopoly_with_third_tier
       :reading_railroad => {}, 
       :pennsylvania_railroad => {}, 
       :b_and_o_railroad => {}, 
-      :
-      
-      
-    
-      
+      :shortline_railroad => {} 
+    }
   }
 }
 end
